1c2ca003328901cfa21ab45c370f07c20ebff6bc
Jialing Zhou <jz5019@ic.ac.uk>
2021-01-18 21:54:49 +0800

    initial implementation of fibonacci package

---
+++ fibonacci/__init__.py
@@ -0,0 +1,5 @@
+from .fibonacci import fib # . means relative import.
+# it means import the "fibonacci" in the same folder as __init__.py
+# so the user only needs to type from fibonacci import fib
+# instead of from fibonacci.fibonacci import fib
+# quit and start a new py intepreter to see the effect
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'week2_fib',
+    version = '0.1',
+    packages = find_packages()
+) # do not forget ',' comma, and name = package name
+# gitignore file will ignore .egg_info
